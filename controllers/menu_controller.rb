409e26307b171a251de82dedb3765bec80fd10d6
vanessa holland <vanessaholland@legalshieldcorp.com>
2016-12-31 12:14:26 -0600

    assignment-6 completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
   def main_menu
     puts "Main Menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View entry by number"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -24,21 +25,23 @@ class MenuController
          main_menu
        when 2
          system "clear"
-         create_entry
+         view_by_number
          main_menu
        when 3
          system "clear"
-         search_entries
+         create_entry
          main_menu
        when 4
          system "clear"
-         read_csv
+         search_entries
          main_menu
        when 5
+         system "clear"
+         read_csv
+         main_menu
+       when 6
          puts "Good-bye!"
-         # #8
          exit(0)
-       # #9
      else
        system "clear"
        puts "Sorry, that is not a valid input"
@@ -51,7 +54,6 @@ class MenuController
      address_book.entries.each do |entry|
        system "clear"
        puts entry.to_s
-     # #15
        entry_submenu(entry)
      end
 
@@ -59,10 +61,25 @@ class MenuController
      puts "End of entries"
    end
 
+   def view_by_number
+     system "clear"
+     valid = false
+     until valid do
+       puts "Enter the entry number you wish to view"
+       num = gets.chomp.to_i
+       if num != 0
+         num -= 1
+         if address_book.entries[num] != nil
+           puts address_book.entries[num].to_s
+           valid = true
+         end
+       end
+     end
+   end
+
    def create_entry
      system "clear"
      puts "New AddressBloc Entry"
-     # #12
      print "Name: "
      name = gets.chomp
      print "Phone number: "
@@ -70,7 +87,6 @@ class MenuController
      print "Email: "
      email = gets.chomp
 
-     # #13
      address_book.add_entry(name, phone, email)
 
      system "clear"
